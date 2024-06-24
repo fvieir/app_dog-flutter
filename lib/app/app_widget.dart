c519923229c1dea00f772bccd73194699adadd6c
Fabricio Dourado <fcvieira2@gmail.com>
2024-06-24 07:32:00 -0300

    feat: Configura asuka e flutter scrrenutil

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:asuka/asuka.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Cuida Pet',
-      theme: ThemeData(primaryColor: Colors.blue),
-      routeInformationParser: Modular.routeInformationParser,
-      routerDelegate: Modular.routerDelegate,
+    Modular.setObservers([Asuka.asukaHeroController]);
+
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp.router(
+        title: 'Cuida Pet',
+        theme: ThemeData(primaryColor: Colors.blue),
+        debugShowCheckedModeBanner: false,
+        builder: Asuka.builder,
+        routeInformationParser: Modular.routeInformationParser,
+        routerDelegate: Modular.routerDelegate,
+      ),
     );
   }
 }
