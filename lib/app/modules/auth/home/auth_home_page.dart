1017fb1048508c036d1256870d22284a770d39f1
Fabricio Dourado <fcvieira2@gmail.com>
2024-06-26 07:32:39 -0300

    feat: Adicionar splashScreen

--- lib/app/modules/auth/home/auth_home_page.dart
+++ lib/app/modules/auth/home/auth_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet_mobile/app/core/ui/extensions/size_screen_util.dart';
 import 'package:flutter/material.dart';
 
 class AuthHomePage extends StatelessWidget {
@@ -6,10 +7,14 @@ class AuthHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Auth'),
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 162.w,
+          height: 130.h,
+          fit: BoxFit.contain,
+        ),
       ),
-      body: Container(),
     );
   }
 }
